214f26ffbe31fee432429829f54387c719102742
Jason Kimmerling <jkimmerling@protonmail.com>
2022-09-22 09:17:48 -0500

    Made the install script a bit more robust

--- README.md
+++ README.md
@@ -5,12 +5,14 @@ My ever changing NixOS configuration
 ## Install
 
 * Install any of the NixOS versions (GUI or CLI)
-* Make sure the internet connection is up
 * Clone the repo into your home directory
+* Change the username in the `configuration.nix` file to whatever your you creted in your NixOS install
 * Run the `setup.sh` script with these provided args
-  * `.setup.sh <username> <channel>`
+  * `./setup.sh <username> <channel>`
     * `<username>` is your user name
     * `<channel>` is the NixOS channel you want to use. 
       * Empty = the default channel in the script
       * Unstable = the unstable branch
       * The exact channel = the exact channel you want to use
+* Run `sudo nixos-rebuild switch`
+* Reboot
--- setup.sh
+++ setup.sh
@@ -1,18 +1,38 @@
-#!/bin/bash
+#!/run/current-system/sw/bin/bash
 
-# get username from variable
-# get channel version (22.05 or Unstable)
+while getopts u:c: flag
+do
+        case "${flag}" in
+            u) username=${OPTARG};;
+            c) channel=${OPTARG};;
+        esac
+done
 
-cd /home/jasonk
+cd /home/$username
 mkdir AppImages
 mkdir Development
 
 git clone git@github.com:jkimmerling/NixOS_DotFiles.git
 
-sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
-sudo nix-channel --update
+if [ ${channel:-0} == 0 ] ; then
+    sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
+    sudo nix-channel --update
+elif [ ${channel:-0} == 'unstable' ]; then
+    sudo nix-channel --remove nixos
+    sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+    sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+    sudo nix-channel --update
+else
+    sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-$channel.tar.gz home-manager
+    sudo nix-channel --update
+    sed -i "s/22.05/$channel" /home/$username/NixOS_DotFiles/nixos/configuration.nix
+fi
 
 sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix-BAK
-sudo ln -s /home/jasonk/NixOS_DotFiles/nixos/configuration.nix /etc/nixos/configuration.nix
+sudo ln -s /home/$username/NixOS_DotFiles/nixos/configuration.nix /etc/nixos/configuration.nix
 
-ln -s /home/jasonk/NixOS_DotFiles/qtile /home/jasonk/.config/qtile
+sed -i "s/jasonk/$username" /home/$username/NixOS_DotFiles/nixos/configuration.nix
+
+ln -s /home/$username/NixOS_DotFiles/qtile /home/$username/.config/qtile
+
+sudo nixos-rebuild switch --upgrade
